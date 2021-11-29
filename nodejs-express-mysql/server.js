2308cd3a7596e153311aead4d9e6cf81014c6352
Linnea Dierksheide <ldierksheide@gwu.edu>
2021-11-29 14:46:01 -0500

    user preferences POST endpoint

--- nodejs-express-mysql/server.js
+++ nodejs-express-mysql/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 const config = require("./config");
 
+
 var con = mysql.createConnection({
     host: config.db.host,
     user: config.db.user,
@@ -31,6 +32,44 @@ app.get("/users", async (req, res) => {
         });
     });
 });
+
+app.get("/categories", async (req, res) => {
+    res.json({ status: "testing users" });
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query("SELECT * FROM categories", function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    });
+});
+
+app.post('/user-pref', (req, res) => {
+    console.log(req.body)
+    //TODO: Should probably check that the user id is valid, that the categories are valid, etc
+    con.connect(function (err) {
+        //TODO: Better error responses
+        if (err) throw err;
+        for(i = 0; i < req.body.pref.length; i++) {
+            var query = "SELECT category_id FROM category WHERE category_name = \"" + req.body.pref[i] + "\";"
+            con.query(query, function (err, result) {
+                // TODO: Better error responses
+                if (err) throw err;
+
+                var rows = JSON.parse(JSON.stringify(result[0]));            
+                query = "INSERT IGNORE INTO user_pref (user_id, category_id) VALUES (" + req.body.user_id + ", " + rows.category_id + ");"
+                con.query(query, function (err, result) {
+                    // TODO: Better error responses
+                    if(err) throw err;
+                })
+            });
+        }
+    });
+
+    // Just returns the Request body and 200 OK status - this could be better
+    res.json(req.body)
+});
+  
 
 module.exports = app;
 
