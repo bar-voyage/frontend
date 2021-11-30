379a649768a8b14a613854c4a42bfb4983b706a2
sreyanalla <sreyanalla@gwu.edu>
2021-11-30 15:14:11 -0500

    implement get bar call
    [BUGFIX] remove mysql multiple conn error

--- nodejs-express-mysql/server.js
+++ nodejs-express-mysql/server.js
@@ -13,7 +13,7 @@ var con = mysql.createConnection({
 const app = express();
 
 app.use(express.json());
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`testing bar voyage db on port ${port}`);
 });
@@ -23,53 +23,54 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/users", async (req, res) => {
-    res.json({ status: "testing users" });
-    con.connect(function (err) {
+    res.json({ status: "getting users" });
+    con.query("SELECT * FROM users", function (err, result) {
         if (err) throw err;
-        con.query("SELECT * FROM users", function (err, result) {
-            if (err) throw err;
-            console.log(result);
-        });
+        console.log(result);
     });
 });
 
 app.get("/categories", async (req, res) => {
-    res.json({ status: "testing users" });
-    con.connect(function (err) {
+    res.json({ status: "getting categories" });
+    con.query("SELECT * FROM category", function (err, result) {
         if (err) throw err;
-        con.query("SELECT * FROM categories", function (err, result) {
-            if (err) throw err;
-            console.log(result);
-        });
+        console.log(result);
     });
 });
 
 app.post('/user-pref', (req, res) => {
     console.log(req.body)
     //TODO: Should probably check that the user id is valid, that the categories are valid, etc
-    con.connect(function (err) {
-        //TODO: Better error responses
-        if (err) throw err;
-        for(i = 0; i < req.body.pref.length; i++) {
-            var query = "SELECT category_id FROM category WHERE category_name = \"" + req.body.pref[i] + "\";"
+    res.json({ status: "posting user prefs" });
+    //     //TODO: Better error responses
+    for (i = 0; i < req.body.pref.length; i++) {
+        var query = "SELECT category_id FROM category WHERE category_name = \"" + req.body.pref[i] + "\";"
+        con.query(query, function (err, result) {
+            // TODO: Better error responses
+            if (err) throw err;
+
+            var rows = JSON.parse(JSON.stringify(result[0]));
+            query = "INSERT IGNORE INTO user_pref (user_id, category_id) VALUES (" + req.body.user_id + ", " + rows.category_id + ");"
             con.query(query, function (err, result) {
                 // TODO: Better error responses
                 if (err) throw err;
-
-                var rows = JSON.parse(JSON.stringify(result[0]));            
-                query = "INSERT IGNORE INTO user_pref (user_id, category_id) VALUES (" + req.body.user_id + ", " + rows.category_id + ");"
-                con.query(query, function (err, result) {
-                    // TODO: Better error responses
-                    if(err) throw err;
-                })
-            });
-        }
-    });
+            })
+        });
+    }
 
     // Just returns the Request body and 200 OK status - this could be better
     res.json(req.body)
 });
-  
+
+app.get('/bars', async (req, res) => {
+    console.log(req.body)
+    res.json({ status: "getting bars" });
+    con.query("SELECT * FROM bar", function (err, result) {
+        if (err) throw err;
+        // con.end();
+        console.log(result);
+    });
+});
 
 module.exports = app;
 
