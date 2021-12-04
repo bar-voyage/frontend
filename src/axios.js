55f52dba9ddd71d682b80aca18cf3a3d1238770b
Christina Ng <christinasy317@gmail.com>
2021-12-04 15:26:49 -0500

    working with aws

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 export const axiosBackendInstance = axios.create({
-  //   baseURL: 'http://ec2-3-16-213-143.us-east-2.compute.amazonaws.com:3000',
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://ec2-3-16-213-143.us-east-2.compute.amazonaws.com:3000',
+  baseURL: 'http://ec2-3-19-223-7.us-east-2.compute.amazonaws.com:3000',
+  // baseURL: 'http://localhost:3000',
   //   timeout: 1000,
 });
